1405246cd389189b1e1cbe418322843d1884d264
Manuel Astudillo <manuel@optimalbits.com>
2014-09-30 11:19:14 +0200

    added a new unit test suite for pathname proxy support

---
+++ test/test_pathnames.js
@@ -0,0 +1,53 @@
+"use strict";
+
+var Redbird = require('../');
+var Promise = require('bluebird');
+var http = require('http');
+var expect = require('chai').expect;
+
+var TEST_PORT = 54673
+var PROXY_PORT = 53432
+
+var opts = {
+  port: PROXY_PORT,
+	bunyan: false /* {
+		name: 'test',
+		streams: [{
+        	path: '/dev/null',
+    	}]
+	} */
+}
+
+describe("Target with pathnames", function(){
+
+	it("Should be proxyed to target with pathname and source pathname concatenated", function(done){
+		var redbird = Redbird(opts);
+
+		expect(redbird.routing).to.be.an("object");
+
+		redbird.register('127.0.0.1', '127.0.0.1:'+TEST_PORT+'/foo/bar/qux');
+
+		expect(redbird.routing).to.have.property("127.0.0.1");
+
+    testServer().then(function(req){
+      expect(req.url).to.be.eql('/foo/bar/qux/a/b/c')
+    })
+
+    http.get('http://127.0.0.1:'+PROXY_PORT+'/a/b/c', function(res) {
+      done();
+    });
+    
+	})
+})
+
+
+function testServer(){
+	return new Promise(function(resolve, reject){
+		var server = http.createServer(function(req, res){
+      res.end();
+			resolve(req);
+			server.close();
+		});
+		server.listen(TEST_PORT);
+	})
+}
