f91b20b361b25926fe05eb19fdc302ed0322425a
Manuel Astudillo <manuel@optimalbits.com>
2024-09-14 23:38:23 +0200

    chore: remove require from letsencrypt

---
+++ lib/declarations.d.ts
@@ -0,0 +1,9 @@
+declare module 'greenlock' {
+  export function create(options: any): any;
+}
+declare module 'le-store-certbot' {
+  export function create(options: any): any;
+}
+declare module 'le-challenge-fs' {
+  export function create(options: any): any;
+}
